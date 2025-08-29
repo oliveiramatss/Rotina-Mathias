1ec6de17916f7436a70237abd4cb642995e97586
oliveiramatss <mateuso64.mo@gmail.com>
2025-08-29 13:48:21 -0300

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ const bodyParser = require('body-parser');
 const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
+
 
 // Middlewares
 app.use(cors());
